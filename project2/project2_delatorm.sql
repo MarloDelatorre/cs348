512e5c36d987b0d58a599dcb6f5f79145d37a2cb
Marlo Delatorre <delatorm@purdue.edu>
2017-10-11 19:05:42 -0400

    Project 2 Question 1

--- project2/project2_delatorm.sql
+++ project2/project2_delatorm.sql
@@ -1,5 +1,57 @@
+-- Question 1
+CREATE OR REPLACE PROCEDURE pro_AvgGrade AS
+	min_bin NUMBER := 0;
+	max_bin NUMBER := 90;
+	CURSOR school_avg IS
+		SELECT sc.SchoolName, AVG(st.grade) AS AverageGrade FROM
+		School sc JOIN Student st ON sc.SchoolId = st.SchoolId
+		GROUP BY sc.SchoolName
+		ORDER BY sc.SchoolName;
+	school_avg_tuple school_avg%ROWTYPE;
+	school_name VARCHAR2(10);
+	school_avg_num NUMBER;
+BEGIN
+	SELECT MIN(low), MAX(high) INTO min_bin, max_bin FROM (
+		SELECT t1.*, FLOOR(AverageGrade / 10) * 10 AS low, FLOOR(AverageGrade / 10) * 10 AS high FROM (
+			SELECT sc.SchoolName, AVG(st.grade) AS AverageGrade FROM
+			School sc JOIN Student st ON sc.SchoolId = st.SchoolId
+			GROUP BY sc.SchoolName
+		) t1
+	);
+	dbms_output.put(RPAD('SCHOOLNAME', 12) || RPAD('AVGGRADE:', 11));
+	FOR i IN min_bin .. max_bin LOOP
+		IF MOD(i, 10) = 0 THEN
+			dbms_output.put('>' || i || ', <=' || (i + 10) ||'  ');
+		END IF;
+	END LOOP;
+	dbms_output.new_line();
+	dbms_output.put(RPAD('----------', 23)); 
+	FOR i IN min_bin .. max_bin loop
+		IF MOD(i, 10) = 0 THEN
+			dbms_output.put(RPAD('---------', 11));
+		END IF;
+	END LOOP;
+	dbms_output.new_line();
+	FOR school_avg_tuple IN school_avg LOOP
+		school_name := school_avg_tuple.SchoolName;
+		school_avg_num := school_avg_tuple.AverageGrade;
+		dbms_output.put(RPAD(school_name, 23));
+		
+		FOR i IN min_bin .. max_bin LOOP
+			IF mod(i, 10) = 0 THEN
+				IF school_avg_num > i AND school_avg_num <= (i + 10) THEN
+					dbms_output.put(RPAD(LPAD('X', 5),5));
+				END IF;	
+				dbms_output.put(RPAD(' ', 11, ' '));
+			END IF;
+		END LOOP;
+		dbms_output.new_line();
+	END LOOP;	
+END;
+/
 
 
+-- Question 2
 CREATE OR REPLACE PROCEDURE pro_DispInternSummary AS
 
 	type freq_array IS VARRAY(100) OF NUMBER;
@@ -75,3 +127,8 @@ BEGIN
 	END LOOP;
 END;
 /
+
+--Question 3
+--Question 4
+--Question 5
+--Question 6
