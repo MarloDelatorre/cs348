98df885e05f38295fc898449090d5f61e1957b17
MarloDelatorre <delatorm@purdue.edu>
2017-10-11 20:55:57 -0400

    Project 2 Question 6

--- project2/project2_delatorm.sql
+++ project2/project2_delatorm.sql
@@ -47,7 +47,7 @@ BEGIN
 		END LOOP;
 		dbms_output.new_line();
 	END LOOP;	
-END;
+END pro_AvgGrade;
 /
 
 
@@ -125,89 +125,31 @@ BEGIN
 		END IF;
 		dbms_output.new_line();
 	END LOOP;
-END;
+END pro_DispInternSummary;
 /
 
 --Question 3
-CREATE OR REPLACE PROCEDURE pro_DispCompany AS
-    cname VARCHAR2(50);
-    caddr VARCHAR(50);
-    nosi NUMBER;
-    cavg NUMBER;
-    undef BOOLEAN := False;
+CREATE OR REPLACE PROCEDURE pro_AddIntern(sname IN Student.StudentName%TYPE, cname IN Company.CompName%TYPE, rname IN Recruiter.RecName%TYPE, OfferYear IN Internship.OfferYear%Type) AS
+	sid NUMBER;
+	cid NUMBER;
+	rid NUMBER;
 BEGIN
-    dbms_output.put_line('CompanyName    Address        NumOfStundentInerns    School    AverageGrade');
-    dbms_output.put_line('-----------    -------        -------------------    ------    ------------');
+	SELECT DISTINCT StudentId INTO sid
+	FROM Student
+	WHERE StudentName = sname;
 
-    FOR comp IN (
-        SELECT CompName, CompId FROM Company
-    ) LOOP
-        BEGIN
-        SELECT CompName, Address, NumOfStudentInterns, AVG(AverageGrade) INTO  cname, caddr, nosi, cavg FROM (
-            SELECT t.CompId, CompName, Address, NVL(t.NumOfStudentInterns, 0) AS NumOfStudentInterns, t.AverageGrade FROM (
-                SELECT t1.SchoolName, t1.CompId, NumOfStudentInterns, AverageGrade FROM (
-                    SELECT sc.SchoolName, i.CompId, AVG(st.Grade) AS AverageGrade FROM
-                    Student st JOIN School sc ON st.SchoolId = sc.SchoolId
-                    JOIN (SELECT DISTINCT StudentId, CompId FROM Internship) i ON st.StudentId = i.StudentId
-                    GROUP BY sc.SchoolName, i.CompId 
-                ) t1 JOIN (
-                    SELECT sc.SchoolName, i.CompId, COUNT(*) AS NumOfStudentInterns FROM
-                    Student st JOIN School sc ON st.SchoolId = sc.SchoolId
-                    JOIN Internship i ON st.StudentId = i.StudentId
-                    GROUP BY i.CompId, sc.SchoolName
-                ) t2 ON t1.SchoolName = t2.SchoolName AND t1.CompId = t2.CompId 
-            ) t RIGHT JOIN Company c ON t.CompId = c.CompId
-            WHERE t.CompId = comp.compId AND NumOfStudentInterns = (SELECT MAX(NumOfStudentInterns) FROM (
-                SELECT * FROM (
-                    SELECT sc.SchoolName, i.CompId, COUNT(*) AS NumOfStudentInterns FROM
-                    Student st JOIN School sc ON st.SchoolId = sc.SchoolId
-                    JOIN Internship i ON st.StudentId = i.StudentId
-                    GROUP BY i.CompId, sc.SchoolName
-                ) WHERE CompId = comp.compId
-            ))
-        )
-        GROUP BY CompId, CompName, Address, NumOfStudentInterns;
-        dbms_output.put(RPAD(cname, 15));
-        dbms_output.put(RPAD(caddr, 20));
-        dbms_output.put(RPAD(nosi, 18));
-        undef := false;
-        EXCEPTION WHEN NO_DATA_FOUND THEN
-            dbms_output.put_line(RPAD(comp.CompName, 35) || 0);
-            undef := True;
-        END;
-        
-        FOR tuple IN (
-            SELECT SchoolName, CompName, Address, NVL(t.NumOfStudentInterns, 0) AS NumOfStudentInterns, t.AverageGrade FROM (
-                SELECT t1.SchoolName, t1.CompId, NumOfStudentInterns, AverageGrade FROM (
-                    SELECT sc.SchoolName, i.CompId, AVG(st.Grade) AS AverageGrade FROM
-                    Student st JOIN School sc ON st.SchoolId = sc.SchoolId
-                    JOIN (SELECT DISTINCT StudentId, CompId FROM Internship) i ON st.StudentId = i.StudentId
-                    GROUP BY sc.SchoolName, i.CompId 
-                ) t1 JOIN (
-                    SELECT sc.SchoolName, i.CompId, COUNT(*) AS NumOfStudentInterns FROM
-                    Student st JOIN School sc ON st.SchoolId = sc.SchoolId
-                    JOIN Internship i ON st.StudentId = i.StudentId
-                    GROUP BY i.CompId, sc.SchoolName
-                ) t2 ON t1.SchoolName = t2.SchoolName AND t1.CompId = t2.CompId 
-            ) t RIGHT JOIN Company c ON t.CompId = c.CompId
-            WHERE t.CompId = comp.compId AND NumOfStudentInterns = (SELECT MAX(NumOfStudentInterns) FROM (
-                SELECT * FROM (
-                    SELECT sc.SchoolName, i.CompId, COUNT(*) AS NumOfStudentInterns FROM
-                    Student st JOIN School sc ON st.SchoolId = sc.SchoolId
-                    JOIN Internship i ON st.StudentId = i.StudentId
-                    GROUP BY i.CompId, sc.SchoolName
-                ) WHERE CompId = comp.compid
-            ))
-        ) LOOP
-            dbms_output.put(RPAD(tuple.SchoolName, 7));
-        END LOOP;
-        IF undef THEN
-            dbms_output.new_line();
-        ELSE
-            dbms_output.put_line(LPAD(cavg, 7));
-        END IF;
-    END LOOP;
-END;
+	SELECT DISTINCT CompId INTO cid
+	FROM Company
+	WHERE CompName = cname;
+
+	SELECT DISTINCT RecId INTO rid
+	FROM Recruiter r
+	WHERE RecName = rname;
+
+	INSERT INTO Internship VALUES(sid, cid, rid, OfferYear);
+END pro_AddIntern;
+/
+
 
 --Question 4
 CREATE OR REPLACE PROCEDURE pro_DispCompany AS
@@ -288,7 +230,77 @@ BEGIN
             dbms_output.put_line(LPAD(cavg, 7));
         END IF;
     END LOOP;
-END;
+END pro_DispCompany;
 
 --Question 5
+CREATE OR REPLACE PROCEDURE pro_SearchStudent(sid NUMBER) AS
+	query sys_refcursor;
+BEGIN
+	OPEN query FOR
+		SELECT st.StudentId, st.StudentName, sc.SchoolName, st.Grade, NumOfInternships, NumOfJobApplications FROM (
+			SELECT st.StudentId, COUNT(*) AS NumOfInternships FROM
+			Student st JOIN Internship i ON st.StudentId = i.StudentId
+			GROUP BY st.StudentId ) ni
+		JOIN (
+			SELECT st.StudentId, COUNT(*) As NumOfJobApplications FROM
+			Student st JOIN JobApplication ja ON st.StudentId = ja.StudentId
+			GROUP BY st.StudentId ) nja
+		ON ni.StudentId = nja.StudentId
+		JOIN Student st ON st.StudentId = ni.StudentId
+		JOIN School sc ON st.SchoolId = sc.SchoolId
+		WHERE st.StudentId = sid;
+
+	dbms_sql.return_result(query, false);
+END pro_SearchStudent;
+/
+
 --Question 6
+CREATE OR REPLACE PROCEDURE pro_SearchRecruiter(rname IN Recruiter.RecName%TYPE) AS
+	rid NUMBER;
+	total_rows NUMBER := 0;
+	companies SYS_REFCURSOR;
+	CURSOR schools(ridd IN NUMBER) IS
+		SELECT SchoolName FROM (
+		SELECT SchoolName, COUNT(*) AS NumOfInterns FROM (
+			SELECT * FROM Internship WHERE RecId = ridd) i JOIN Student st ON i.StudentId = st.StudentId
+			JOIN School sc ON st.SchoolId = sc.SchoolId
+		GROUP BY SchoolName
+	
+	)
+	WHERE NumOfInterns = (SELECT MAX(NumOfInterns) FROM (
+		SELECT SchoolName, COUNT(*) AS NumOfInterns FROM (
+		SELECT * FROM Internship WHERE RecId = ridd) i JOIN Student st ON i.StudentId = st.StudentId
+		JOIN School sc ON st.SchoolId = sc.SchoolId
+	GROUP BY SchoolName
+	));
+	schools_tuple schools%ROWTYPE;
+BEGIN
+	SELECT RecId INTO rid
+	FROM Recruiter
+	WHERE RecName = rname;
+
+	dbms_output.put_line('RecID: ' || rid);
+	dbms_output.put_line('RecName: ' || rname);
+	dbms_output.put('School with most interns: ');
+
+	FOR schools_tuple IN schools(rid) LOOP
+		total_rows := total_rows + 1;
+	END LOOP;
+
+	FOR schools_tuple IN schools(rid) LOOP
+		dbms_output.put(schools_tuple.SchoolName);
+		IF schools%ROWCOUNT != total_rows THEN
+			dbms_output.put(' / ');		
+		END IF;
+	END LOOP;
+	dbms_output.new_line();
+
+	OPEN companies FOR 
+		SELECT CompName, COUNT(*) AS NumberOfInterns, TRUNC(AVG(st.Grade), 2) AS AverageStudentGrade FROM
+		Internship i JOIN Student st ON i.StudentId = st.StudentId
+		JOIN Company c on c.CompId = i.CompId
+		WHERE i.RecId = rid
+		GROUP BY c.CompName;
+
+	dbms_sql.return_result(companies);
+END pro_SearchRecruiter;
